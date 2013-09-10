b5dc8751d4ac1195c863401586bda01ed2fb4803
Martin Ortbauer <mortbauer@gmail.com>
2013-09-10 15:03:06 +0200

    formatts the result now correctly

--- asterclient/translator.py
+++ asterclient/translator.py
@@ -9,10 +9,11 @@ def translator_translate(text,sl,tl):
     params = {'sl':sl,'tl':tl,'text':text}
     headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'}
     request = requests.get('http://www.translate.google.com',params=params,headers=headers)
-    feeddata = request.content
+    feeddata = request.text
     soup = BeautifulSoup(feeddata)
     try:
-        return(soup.find('span', id="result_box").text.encode("utf-8"))
+        result = soup.find('span', id="result_box")
+        return '\n'.join([x.get('title') for x in result.contents])
     except:
         return('No result found')
 
