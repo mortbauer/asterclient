b0f114b737cae90b152062d7e74f2575aaae2c06
Martin Ortbauer <mortbauer@gmail.com>
2013-09-13 17:49:29 +0200

    fixed the setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,6 @@ import sys
 import asterclient
 from setuptools import setup
 
-# from http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy
-def parse_requirements(file_name):
-    requirements = []
-    for line in open(file_name, 'r').read().split('\n'):
-        if re.match(r'(\s*#)|(\s*$)', line):
-            continue
-        if re.match(r'\s*-e\s+', line):
-            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
-        elif re.match(r'\s*-f\s+', line):
-            pass
-        else:
-            requirements.append(line)
-
-    return requirements
-
-
 def parse_dependency_links(file_name):
     dependency_links = []
     for line in open(file_name, 'r').read().split('\n'):
@@ -44,7 +28,6 @@ setup(name='asterclient',
       license=asterclient.__copyright__,
       packages=['asterclient'],
       package_data={'asterclient':['data/defaults.yml']},
-      install_requires=parse_requirements('requirements.txt'),
       dependency_links=parse_dependency_links('requirements.txt'),
       extras_require = {
         'autofigure':  ["matplotlib"]
